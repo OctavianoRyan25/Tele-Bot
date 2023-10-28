d616fc3de0873788b151119868424fa0b5888104
OctavianoRyan25 <octavianoryan25@gmail.com>
2023-10-28 10:49:48 +0700

    first commit

---
+++ src/main/java/com/mycompany/uas_pbo/UAS_PBO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas_pbo;
+
+import Auth.Login;
+
+/**
+ *
+ * @author octav
+ */
+public class UAS_PBO {
+
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+    }
+}
